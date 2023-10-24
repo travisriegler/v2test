42dcea1bdec1c56071b1d9e4e2f4eff5360aea53
Travis Riegler <travisriegler@protonmail.com>
2023-10-24 09:10:59 -0400

    initial commit to main branch

---
+++ src/main/java/com/example/v2test/V2testApplication.java
@@ -0,0 +1,13 @@
+package com.example.v2test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class V2testApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(V2testApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/v2test/V2testApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.v2test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class V2testApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
